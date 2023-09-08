730b550ec517966fdbe7eea02c026665d70c96fd
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-08 10:35:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function secondHighest(arr) {
-  if (arr.length < 2) {
-    return -Infinity; // Not enough elements
+  if (arr.length <= 1) {
+    return -Infinity;
   }
 
   // Sort the array in descending order
   arr.sort((a, b) => b - a);
 
-  // Iterate through the sorted array to find the second highest element
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] !== arr[i - 1]) {
-      return arr[i]; // Found the second highest element
-    }
+  // Find the second element in the sorted array
+  const secondHighestElement = arr[1];
+
+  if (secondHighestElement === arr[0]) {
+    return -Infinity; // All elements are the same
   }
 
-  return -Infinity; // All elements are the same
+  return secondHighestElement;
 }
 
 // Example usage:
 console.log(secondHighest([5, 1, 2, 3, 4]));    // Output: 4
-console.log(secondHighest([-1, -2, -3, -4, -5]));// Output: -2
+console.log(secondHighest([-1, -2, -3, -4, -5])); // Output: -2
 console.log(secondHighest([]));                 // Output: -Infinity
 console.log(secondHighest([1]));                // Output: -Infinity
 console.log(secondHighest([1, 1, 1, 1, 1]));    // Output: -Infinity
+
