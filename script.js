e560e286ecbbaf9b6af370c18814313dee23f61c
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-13 07:30:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 function secondHighest(arr) {
-  if (arr.length <= 1) {
-    return -Infinity;
-  }
+    if (arr.length < 2) {
+        return -Infinity;
+    }
 
-  // Sort the array in descending order
-  arr.sort((a, b) => b - a);
+    // Sort the array in descending order
+    arr.sort((a, b) => b - a);
 
-  // Find the second element in the sorted array
-  const secondHighestElement = arr[1];
+    // Find the second element in the sorted array
+    const secondHighestElement = arr[1];
 
-  if (secondHighestElement === arr[0]) {
-    return -Infinity; // All elements are the same
-  }
-
-  return secondHighestElement;
+    return secondHighestElement;
 }
 
 // Example usage:
-console.log(secondHighest([5, 1, 2, 3, 4]));    // Output: 4
-console.log(secondHighest([-1, -2, -3, -4, -5])); // Output: -2
-console.log(secondHighest([]));                 // Output: -Infinity
-console.log(secondHighest([1]));                // Output: -Infinity
-console.log(secondHighest([1, 1, 1, 1, 1]));    // Output: -Infinity
+const arr1 = [5, 1, 2, 3, 4];
+const arr2 = [-1, -2, -3, -4, -5];
+const arr3 = [];
+const arr4 = [1];
+const arr5 = [1, 1, 1, 1, 1];
 
+console.log(secondHighest(arr1)); // Output: 4
+console.log(secondHighest(arr2)); // Output: -2
+console.log(secondHighest(arr3)); // Output: -Infinity
+console.log(secondHighest(arr4)); // Output: -Infinity
+console.log(secondHighest(arr5)); // Output: -Infinity
